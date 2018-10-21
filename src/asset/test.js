d2734096bdcf517379e016f436bbf424499b3090
Thangse <mt.tranthang@gmail.com>
2018-10-21 12:29:36 +0700

    Upload Gitignore

--- src/asset/test.js
+++
@@ -1,9 +0,0 @@
-{
-    type: "webhook-outgoing",
-    "name": "Testing via REST API",
-    "event": "sendMessage",
-    "enabled": false,
-    "username": "rocket.cat",
-    "urls": ["http://text2gif.guggy.com/guggify"],
-    "scriptEnabled": false
-}
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import CpmUploadFile from './components/cpmUploadFile'
-
-ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
